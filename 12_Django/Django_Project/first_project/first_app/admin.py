36c77248083320c5bd0a1c22110e630614fcaed4
Ignacio Rognoni <rognoni.ignacio@gmail.com>
2017-11-18 22:58:39 -0300

    [Course Lesson] Django Level Two - Creating Models

--- 12_Django/Django_Project/first_project/first_app/admin.py
+++ 12_Django/Django_Project/first_project/first_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from first_app.models import Topic, Webpage, AccessRecord
 # Register your models here.
+
+admin.site.register(Topic)
+admin.site.register(Webpage)
+admin.site.register(AccessRecord)
