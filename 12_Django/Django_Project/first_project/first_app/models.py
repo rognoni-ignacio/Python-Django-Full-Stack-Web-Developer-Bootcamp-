b7d29c730d301fa9a22e4521ebf0d306a54d4a40
Ignacio Rognoni <rognoni.ignacio@gmail.com>
2017-11-19 15:11:22 -0300

    [Course Lesson] Django Level Two - MTV Paradigm

--- 12_Django/Django_Project/first_project/first_app/models.py
+++ 12_Django/Django_Project/first_project/first_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Topic(models.Model):
     top_name = models.CharField(max_length=264, unique=True)
 
--- 12_Django/Django_Project/first_project/first_app/views.py
+++ 12_Django/Django_Project/first_project/first_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import AccessRecord, Topic, Webpage
 # Create your views here.
 
+
 def index(request):
-    my_dict = {'insert_me': 'Hello World!'}
-    return render(request, 'first_app/index.html', context=my_dict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
+    return render(request, 'first_app/index.html', context=date_dict)
