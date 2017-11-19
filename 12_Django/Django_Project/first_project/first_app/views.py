6555d4dd84bd31d1e186d8f199ec83d62db3fcfe
Ignacio Rognoni <rognoni.ignacio@gmail.com>
2017-11-18 21:29:47 -0300

    [Course Lesson] Django Level One - Part Seven - Templates

--- 12_Django/Django_Project/first_project/first_app/views.py
+++ 12_Django/Django_Project/first_project/first_app/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
+    my_dict = {'insert_me': 'Hello World!'}
+    return render(request, 'first_app/index.html', context=my_dict)
--- 12_Django/Django_Project/first_project/first_project/settings.py
+++ 12_Django/Django_Project/first_project/first_project/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'first_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
