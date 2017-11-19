b97e9156f91c27042629c33927c0241497849c0a
Ignacio Rognoni <rognoni.ignacio@gmail.com>
2017-11-18 22:29:29 -0300

    [Course Lesson] Django Level One - Part Ten - Static Files

--- 12_Django/Django_Project/first_project/first_project/settings.py
+++ 12_Django/Django_Project/first_project/first_project/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
