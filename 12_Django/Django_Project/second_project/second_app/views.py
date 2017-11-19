7a0a58a0b53ea2a98daa7dc626467bd7ee0292b6
Ignacio Rognoni <rognoni.ignacio@gmail.com>
2017-11-18 21:41:38 -0300

    [Course Lesson] Django Level One - Part 8 and 9 - Templates Challenge

--- 12_Django/Django_Project/second_project/second_app/views.py
+++ 12_Django/Django_Project/second_project/second_app/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<em>My Second App</em>")
+
+
+def help(request):
+    my_dict = {"help_text": "This is the help page!"}
+    return render(request, 'second_app/help.html', context=my_dict)
--- 12_Django/Django_Project/second_project/second_project/urls.py
+++ 12_Django/Django_Project/second_project/second_project/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from second_app import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^help/', include('second_app.urls')),
     url(r'^admin/', admin.site.urls),
 ]
