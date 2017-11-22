4cd64a1cf9139953e485c524d3656b8ad1fc7490
Ignacio Rognoni <rognoni.ignacio@gmail.com>
2017-11-22 19:47:37 -0300

    [Course Lesson] Django Level Five - Coding User Models and Forms - Part One

---
+++ 12_Django/Level_5/first_project/first_app/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from djang.contrib.auth.models import User
+from first_app.models import UserProfileInfo
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta():
+        model = User
+        fields = ('username', 'email', 'password')
+
+
+class UserProfileInfoForm(forms.ModelForm):
+    class Meta():
+        model = UserProfileInfo
+        fields = ('portfolio_site', 'profile_picture')
--- 12_Django/Level_5/first_project/first_app/models.py
+++ 12_Django/Level_5/first_project/first_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User)
+    profile_picture = models.ImageField(upload_to='profile-pictures', blank=True)
+    portfolio_site = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.user.username
