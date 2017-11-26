de27d64266831f2a1c47aa635386d908e26cf7a0
Ignacio Rognoni <rognoni.ignacio@gmail.com>
2017-11-26 15:23:59 -0300

    [Clone Project] Added comments for blog

--- 12_Django/Clone_Projects/blog/blog_project/blog_app/admin.py
+++ 12_Django/Clone_Projects/blog/blog_project/blog_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from blog_app.models import Author, Comment, Post
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Comment)
+admin.site.register(Post)
--- 12_Django/Clone_Projects/blog/blog_project/blog_app/models.py
+++ 12_Django/Clone_Projects/blog/blog_project/blog_app/models.py
@@ -7,17 +7,47 @@ from django.contrib.auth.models import User
 class Author(models.Model):
     user = models.OneToOneField(User)
     personal_site = models.URLField(blank=True)
-    profile_picture = models.ImageField(upload_to='profile-pictures', blank=True)
+    profile_picture = models.ImageField(
+        upload_to='profile-pictures', blank=True)
 
     def __str__(self):
         return self.user.username
 
 
 class Post(models.Model):
-    name = models.CharField(max_length=256)
+    title = models.CharField(max_length=256)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
+    publish_date = models.DateTimeField(blank=True, null=True)
     author = models.ForeignKey(Author, related_name='authors')
 
+    def publish(self):
+        self.publish_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={'pk': self.pk})
+
     def __str__(self):
-        return self.name
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse("post_lists")
+
+    def __str__(self):
+        return self.text
