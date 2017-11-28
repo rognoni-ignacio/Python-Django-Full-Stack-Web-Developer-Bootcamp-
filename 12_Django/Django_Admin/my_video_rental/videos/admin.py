4f5efd99494eb32e79e96d922269e78d10a4c9b9
Ignacio Rognoni <rognoni.ignacio@gmail.com>
2017-11-27 23:58:14 -0300

    [Course Lesson] Customizing the Django Admin - Adding Filters

--- 12_Django/Django_Admin/my_video_rental/videos/admin.py
+++ 12_Django/Django_Admin/my_video_rental/videos/admin.py
@@ -9,6 +9,8 @@ class MovieAdmin(admin.ModelAdmin):
 
     search_fields = ['title', 'length']
 
+    list_filter = ['release_year', 'length']
+
 
 admin.site.register(models.Customer)
 admin.site.register(models.Movie, MovieAdmin)
