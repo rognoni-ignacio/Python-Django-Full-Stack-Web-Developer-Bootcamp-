b82b78fb4ce6f3add94eae2fecdef3ae4766fe8e
Ignacio Rognoni <rognoni.ignacio@gmail.com>
2017-11-27 23:54:44 -0300

    [Course Lesson] Customizing the Django Admin - Ordering Fields

--- 12_Django/Django_Admin/my_video_rental/videos/admin.py
+++ 12_Django/Django_Admin/my_video_rental/videos/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+
+# NAME -> THE NAME OF THE MODEL + ADMIN
+class MovieAdmin(admin.ModelAdmin):
+    fields = ['title', 'release_year', 'length']
+
+
 admin.site.register(models.Customer)
-admin.site.register(models.Movie)
+admin.site.register(models.Movie, MovieAdmin)
