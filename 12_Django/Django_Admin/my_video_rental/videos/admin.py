83a646e97354a1c96d6c4420f46b07bab232693d
Ignacio Rognoni <rognoni.ignacio@gmail.com>
2017-11-28 00:00:05 -0300

    [Course Lesson] Customizing the Django Admin - Adding Fields

--- 12_Django/Django_Admin/my_video_rental/videos/admin.py
+++ 12_Django/Django_Admin/my_video_rental/videos/admin.py
@@ -11,6 +11,8 @@ class MovieAdmin(admin.ModelAdmin):
 
     list_filter = ['release_year', 'length']
 
+    list_display = ['title', 'release_year', 'length']
+
 
 admin.site.register(models.Customer)
 admin.site.register(models.Movie, MovieAdmin)
