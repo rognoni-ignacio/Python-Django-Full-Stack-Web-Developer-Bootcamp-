2ccfa66ebb1c0ce41346c5f71f4f635e2ce064e0
Ignacio Rognoni <rognoni.ignacio@gmail.com>
2017-11-29 23:32:30 -0300

    [Clone Project] Social Clone - Part Seven

--- 12_Django/Clone_Projects/social_network/simplesocial/groups/views.py
+++ 12_Django/Clone_Projects/social_network/simplesocial/groups/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.core.urlresolvers import reverse
+from django.views.generic import CreateView, DetailView, ListView
+from groups.models import Group, GroupMember
 
-# Create your views here.
+
+class CreateGroupView(LoginRequiredMixin, CreateView):
+    fields = ('name', 'description')
+    model = Group
+
+
+class SingleGroup(DetailView):
+    model = Group
+
+
+class ListGroup(ListView):
+    model = Group
