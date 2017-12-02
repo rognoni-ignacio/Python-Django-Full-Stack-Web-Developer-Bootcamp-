8984617e10ed6bf60bef949be72092c42ecbcb40
Ignacio Rognoni <rognoni.ignacio@gmail.com>
2017-12-01 23:19:03 -0300

    [Clone Project] Social Clone - Final Part

--- 12_Django/Clone_Projects/social_network/simplesocial/simplesocial/settings.py
+++ 12_Django/Clone_Projects/social_network/simplesocial/simplesocial/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
